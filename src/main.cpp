8df2ea98013ddbd5757c85003d27997313c01488
deacardos <102183309+deacardos@users.noreply.github.com>
2022-04-30 14:04:14 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "kofe.h"
+
+using std::string;
+
+int main() {
+    kofe machine = kofe();
+    machine.on();
+    machine.getMenu();
+    machine.coin(20);
+    machine.choice(1);
+    machine.finish();
+    machine.coin(10);
+    machine.coin(20);
+    machine.choice(3);
+    machine.finish();
+    machine.off();
+    return 0;
+}
