d162a5dda9ca2589e3d67dfdd4532ab402e51f90
deacardos <102183309+deacardos@users.noreply.github.com>
2022-04-30 13:56:16 +0300

    Create kofe.h

---
+++ include/kofe.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+enum States {
+  OFF,
+  WAIT,
+  ACCEPT,
+  CHECK,
+  COOK
+};
+
+class Automata {
+ private:
+    int cash;
+    string* menu;
+    int* prices;
+    States state;
+    int option;
+
+ public:
+    Automata();
+    void on();
+    void off();
+    void coin(int money);
+    void getMenu();
+    States getState();
+    void choice(int _choice);
+    int cancel();
+    bool check();
+    void cook();
+    int finish();
+};
